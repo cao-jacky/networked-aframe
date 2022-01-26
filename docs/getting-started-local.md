89166c59edb00f909710bf4c4aab25ce10098f2c
Jacky Cao <jackycao@outlook.com>
2022-01-26 13:27:49 +0000

    backing up work until 20220126

--- docs/getting-started-local.md
+++ docs/getting-started-local.md
@@ -36,7 +36,7 @@ Now let's setup the required dependencies. Create a file called `package.json` a
   },
   "author": "YOUR_NAME",
   "dependencies": {
-    "networked-aframe": "^0.7.0"
+    "networked-aframe": "^0.8.0"
   }
 }
 ```
@@ -88,6 +88,21 @@ http://localhost:8080
 
 You should see an index page that shows a list of the included examples.
 
+In the html files, you need to change the script tag
+
+```html
+<script src="/dist/networked-aframe.js"></script>
+```
+
+to
+
+```html
+<script src="https://unpkg.com/networked-aframe@^0.8.0/dist/networked-aframe.min.js"></script>
+```
+
+for the examples to work.
+
+
 <img src="http://i.imgur.com/pc07Nyir.png" width="500">
 
 To see the networking in action, open up two browser windows and go to the same example on both. Click and hold with the mouse to move your avatar's head around. The other browser should see your avatar's movements.
@@ -111,7 +126,7 @@ Here's the template we'll start with:
     <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js"></script>
     <script src="/easyrtc/easyrtc.js"></script>
-    <script src="https://unpkg.com/networked-aframe/dist/networked-aframe.min.js"></script>
+    <script src="https://unpkg.com/networked-aframe@^0.8.0/dist/networked-aframe.min.js"></script>
   </head>
   <body>
     <a-scene></a-scene>
@@ -119,8 +134,10 @@ Here's the template we'll start with:
 </html>
 ```
 
-Please don't use `https://unpkg.com/networked-aframe/dist/networked-aframe.min.js` for production, it will get the latest release.
-For production you want to pin to a specific version like `https://unpkg.com/networked-aframe@0.8.0/dist/networked-aframe.min.js`.
+Please don't use `https://unpkg.com/networked-aframe/dist/networked-aframe.min.js` for production, this will download the latest major release that may contain breaking changes.
+It's ok for a testing environment to specify "@^0.8.0" in the url so it downloads the latest minor version that shouldn't have breaking changes.
+For production you want to pin to a specific version like `https://unpkg.com/networked-aframe@0.8.3/dist/networked-aframe.min.js`.
+
 If you want to use a more recent build from github master that is not released yet, you can use:
 
 ```html
---
+++ src/components/networked-video-sourceed.js
@@ -0,0 +1,103 @@
+/* global AFRAME, NAF, THREE */
+var naf = require('../NafIndex');
+
+AFRAME.registerComponent('networked-video-sourceed', {
+
+  schema: {
+    streamName: { default: 'video' },
+  },
+
+  dependencies: ['material'],
+
+  init: function () {
+    this.videoTexture = null;
+    this.video = null;
+    this.stream = null;
+    this.isvideoSet = false;
+    this._setMediaStream = this._setMediaStream.bind(this);
+    this.ownerID='';
+    NAF.utils.getNetworkedEntity(this.el).then((networkedEl) => {
+      const ownerId = networkedEl.components.networked.data.owner;
+        this.ownerID = ownerId;
+      if (ownerId) {
+        NAF.connection.adapter.getMediaStream(ownerId, this.data.streamName)
+          .then(this._setMediaStream)
+          .catch((e) => naf.log.error(`Error getting media stream for ${ownerId}`, e));
+      } else {
+          console.log('_setMediaStream done');
+        // Correctly configured local entity, perhaps do something here for enabling debug audio loopback
+      }
+    });
+  },
+
+  _setMediaStream(newStream) {
+
+    if(!this.video) {
+      this.setupVideo();
+    }
+
+    if(newStream != this.stream) {
+      if (this.stream) {
+    //    this._clearMediaStream();
+      }
+
+      if (newStream) {
+        this.video.srcObject = newStream;
+        //  this.video.srcObject = navigator.allMediaStreams[0];
+        const playResult = this.video.play();
+        if (playResult instanceof Promise) {
+          playResult.catch((e) => naf.log.error(`Error play video stream`, e));
+        }
+
+        if (this.videoTexture) {
+          //this.videoTexture.dispose();
+        }
+
+        this.videoTexture = new THREE.VideoTexture(this.video);
+
+        const mesh = this.el.getObject3D('mesh');
+        mesh.material.map = this.videoTexture;
+        mesh.material.needsUpdate = true;
+      }
+
+      this.stream = newStream;
+    }
+  },
+
+  _clearMediaStream() {
+
+    this.stream = null;
+
+    if (this.videoTexture) {
+
+      if (this.videoTexture.image instanceof HTMLVideoElement) {
+        // Note: this.videoTexture.image === this.video
+        //const video = this.videoTexture.image;
+        //video.pause();
+        //video.srcObject = null;
+        //video.load();
+      }
+
+//      this.videoTexture.dispose();
+  //    this.videoTexture = null;
+    }
+  },
+
+  remove: function() {
+      this._clearMediaStream();
+  },
+
+  setupVideo: function() {
+     while  (!this.video) {
+         const video = document.createElement('video');
+         video.setAttribute('id', this.ownerID);
+         video.setAttribute('autoplay', true);
+         video.setAttribute('playsinline', true);
+         video.setAttribute('muted', true);
+         this.video = video;
+         document.body.appendChild(video);
+    }
+ }, tick: function () {
+      
+    }
+});
